e457fa8a8f1af9e58a0fdeec7c6d387a446321d8
mfrb15 <129767020+mfrb15@users.noreply.github.com>
2023-08-14 10:54:18 -0400

    Create orders and login components and configure adds them to the router

--- DEV005-burger-queen-api-client/src/app/app-routing.module.ts
+++ DEV005-burger-queen-api-client/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { LoginComponent } from './login/login.component';
+import { OrdersComponent } from './orders/orders.component';
+// designa las constantes de las rutas y las define.
+const routes: Routes = [
+  { path: 'login-component', component: LoginComponent },
+  { path: 'orders-component', component: OrdersComponent }
+];
+// Configura los exports e imports de NgModule
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
